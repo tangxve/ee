1f2403f6adb4f6aeb9154d2c7a4419a5d03d246a
tangxve <11183504@qq.com>
2023-01-18 18:42:58 +0800

    feat: 添加依赖

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+
+import { Button, Space } from 'antd'
+import { useState } from 'react'
+import styled from 'styled-components'
+
+import reactLogo from './assets/react.svg'
+
+const Wrapper = styled.section`
+  margin: 0 auto;
+  width: 300px;
+  text-align: center;
+`
+const ButtonA = styled.button`
+  width: 100px;
+  background: skyblue;
+  color: red;
+`
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Space wrap>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+      </Space>
+      <Wrapper>
+        <ButtonA>Hello World</ButtonA>
+      </Wrapper>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
