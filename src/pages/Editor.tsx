61f77cf5cf556c94f3230ffcf918b56c3f530917
tangxve <11183504@qq.com>
2023-01-25 22:25:57 +0800

    feat: 调整 tailwindcss 提示

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,5 +1,43 @@
+import React from 'react'
+import { Layout } from 'antd'
+
+const { Header, Sider, Content } = Layout
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+}
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+}
+
+const siderStyle: React.CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#3ba0e9',
+}
+
 export default function Editor() {
   return (
-    <div>editor</div>
+    <div className="h-screen">
+      <Layout className="h-screen">
+        <Header style={headerStyle}>Header</Header>
+        <Layout>
+          <Sider style={siderStyle}>Sider</Sider>
+          <Content style={contentStyle}>Content</Content>
+          <Sider style={siderStyle}>Sider</Sider>
+        </Layout>
+      </Layout>
+    </div>
   )
 }
