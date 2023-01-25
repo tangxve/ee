338f0d94038f70f5f570de9588848954462abf78
tangxve <11183504@qq.com>
2023-01-25 21:27:25 +0800

    feat: 调整路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,14 @@
 import type { RouteObject } from 'react-router-dom'
-import { Navigate } from 'react-router-dom'
-
 import Editor from '../pages/Editor'
 
 const router: RouteObject[] = [
   {
     path: '/',
-    element: <Navigate to="/editor" />,
-    children: [
-      {
-        path: 'editor',
-        element: <Editor />,
-      },
-    ],
+    element: <Editor/>,
+  },
+  {
+    path: '/editor',
+    element: <Editor/>,
   },
 ]
-
 export default router
